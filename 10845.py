e3e521cfe58227f761076d9e94d1d47a577ba7f0
funnykyeon@naver.com <funnykyeon@naver.com>
2022-04-01 23:15:15 +0900

    10845

--- 10845.py
+++ 10845.py
@@ -1,31 +1,39 @@
-import sys
+from sys import stdin
 
-N = int(sys.stdin.readline())
-
-queue = []
-
+N = int(stdin.readline())
+Que = []
 for i in range(N):
-    cmd = sys.stdin.readline().split()
+    A = stdin.readline().split() # push 명령어 입력시 공백으로 split 구분 후 뒤에 정수만 넣기 위함
 
-    if cmd[0] == "push":
-        queue.insert(0, cmd[1])
-        ##print(queue)
+    if A[0] == 'push':
+        Que.append(A[1])
 
-    elif cmd[0] == "pop":
-        if len(queue) != 0: print(queue.pop())
-        else: print(-1)
+    elif A[0] == 'pop':
+        if Que:
+            # deque에서 pop할땐  popleft()를 사용해야함
+            print(Que.pop(0))
+        else:
+            print(-1)
 
-    elif cmd[0] == "size":
-        print(len(queue))
+    elif A[0] == 'size':
+        # len으로 Que개수 확인
+        print(len(Que))
 
-    elif cmd[0] == "empty":
-        if len(queue) == 0: print(1)
-        else : print(0)
+    elif A[0] == 'empty':
+        if len(Que) == 0:
+            print(1)
+        else:
+            print(0)
 
-    elif cmd[0] == "front":
-        if len(queue) == 0: print(-1)
-        else: print(queue[len(queue) -1])
+    elif A[0] == 'front':
+        if len(Que) == 0:
+            print(-1)
+        else:
+            print(Que[0])
+    #       Que가 비어있지않다면 Que에 0번째(front) 출력
 
-    elif cmd[0] == "back":
-        if len(queue) == 0: print(-1)
-        else: print(queue[0])
+    elif A[0] == 'back':
+        if len(Que) == 0:
+            print(-1)
+        else:
+            print(Que[-1])
