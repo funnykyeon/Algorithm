bc9d4b8fe077d6553981beed97533c768ee685c2
kiwon kwon <funnykyeon@naver.com>
2022-05-06 09:00:43 +0900

    3052

---
+++ AlgorithmStudy/1차/2839.py
@@ -0,0 +1,11 @@
+N = int(input())
+count = 0
+while True:
+    if N % 5 == 0:
+        print(count + N // 5)
+        break
+    N -= 3
+    count += 1
+    if N < 0:
+        print(-1)
+        break
